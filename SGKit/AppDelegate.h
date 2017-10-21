52f551ec5eb11b35fd945ff0a4d3828ec4751f7d
chenxiaosong <chenxiaosong@chenxiaosongdemacbook-pro.local>
2017-10-22 07:54:27 +0800

    init commit

---
+++ SGKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SGKit
+//
+//  Created by chenxiaosong on 2017/10/21.
+//  Copyright © 2017年 chenxiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SGKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SGKit
+//
+//  Created by chenxiaosong on 2017/10/21.
+//  Copyright © 2017年 chenxiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
